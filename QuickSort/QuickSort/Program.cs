86d0cee0b8ea3c612d61428d132132c09916d41a
shardul vikram <shardul@outlook.com>
2014-04-03 21:56:45 -0700

    naive quick sort tool

---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Enter numbers ");
+                var inputLine = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(inputLine))
+                {
+                    Console.WriteLine("Invalid input, try again");
+                    continue;
+                }
+
+                var items = inputLine.Split(
+                    new[]
+                    {
+                        ' ',
+                        '\t'
+                    },
+                    StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                var sortedItems = QuickSortProvider.NaiveSort(items).ToList();
+                sortedItems.ForEach(item =>  Console.Write(string.Format("{0} ", item)));
+                Console.WriteLine();
+
+                Console.WriteLine("Enter space to continue, another key to quit");
+                var inp = Console.ReadKey().Key;
+                if (inp != ConsoleKey.Spacebar)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ QuickSort/QuickSort/QuickSortProvider.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    public static class QuickSortProvider
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="itemList"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> NaiveSort<T>(T[] itemList) where T : IComparable
+        {
+            int numberOfElements = itemList.Count();
+
+            if (!itemList.Any() || numberOfElements == 1)
+            {
+                return itemList;
+            }
+
+            int pivot = numberOfElements / 2;
+            var leftArray = itemList.Where((item, i) =>  i != pivot && item.CompareTo(itemList[pivot]) <= 0);
+            var rightArray = itemList.Where((item, i) => i != pivot && item.CompareTo(itemList[pivot]) > 0);
+
+            return Concat(NaiveSort(leftArray.ToArray()).ToList(),  itemList[pivot], NaiveSort(rightArray.ToArray()).ToList());
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array1"></param>
+        /// <param name="item"></param>
+        /// <param name="array2"></param>
+        /// <returns></returns>
+        private static IEnumerable<T> Concat<T>(List<T> array1, T item, List<T> array2) where T : IComparable
+        {
+            array1.Add(item);
+            array1.AddRange(array2);
+            return array1;
+        }
+
+    }
+}
